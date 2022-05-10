508d4299feeb6ec169d71b27613da35b8f6c6254
Eve Fernando <eve@eandshome.net>
2022-05-10 23:00:54 +1000

    Adds docs

--- analytics.py
+++ analytics.py
@@ -1,12 +1,40 @@
 from analytics_helper import *
 import random
 from datetime import datetime
+from typing import List
+import pandas as pd
 
 random.seed(3888)
 
 countries_data = integrate_all_data()
 
-def generate_country_df(country, covid_concern, cost, continents, interests):
+
+def generate_country_df(country: str, covid_concern: str, cost: str, continents: List[str], interests: List[str]) -> pd.DataFrame:
+    """Generates a Pandas DataFrame containing information about a particular country, as well as its 5 nearest neighbours.
+
+    Args:
+        country (str): Country selected by user.
+        covid_concern (str): Can be one of the following values: {'low', 'medium', 'high'}, depending on user input.
+        cost (str): Can be one of the following values: {'budget', 'mid-range', 'luxury'}, depending on user input.
+        continents (List[str]): Can be any of the continent names in the OWID dataset (e.g., 'Asia', 'Europe').
+        interests (List[str]): Variable group(s) selected by user in UI.
+                               Variable group options:
+                               - covid
+                               - infrastructure_quality_and_availability
+                               - health_and_safety
+                               - cost
+                               - food
+                               - places_of_worship
+                               - indoor_attractions
+                               - outdoor_attractions
+                               - nature
+                               - nightlife
+                               - shopping
+                               - relaxation
+
+    Returns:
+        pandas.DataFrame: DataFrame containing information about a particular country, as well as its 5 nearest neighbours.
+    """
     cols_of_interest = convert_interests_to_cols(interests)
     weightings = generate_feature_weightings_dict(cols_of_interest)
 
@@ -17,4 +45,5 @@ def generate_country_df(country, covid_concern, cost, continents, interests):
 
     return None
 
-print(generate_country_df("France", 'low', 'luxury', ['Europe', 'Oceania'], ['covid']))
+# Example usage of function above
+# print(generate_country_df("France", 'low', 'luxury', ['Europe', 'Oceania'], ['covid']))
