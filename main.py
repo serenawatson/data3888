6c08c91e962964f517dc88c1cbb7c3d1aab042aa
mmon7536 <mmon7536@uni.sydney.edu.au>
2022-05-10 20:32:38 +1000

    UI prototype

--- main.py
+++ main.py
@@ -30,98 +30,207 @@ attraction_types = ['amusement_park', 'aquarium', 'art_gallery', 'bar', 'book_st
                     'park', 'parking', 'place_of_worship', 'restaurant', 'shopping_mall', 'spa',
                     'store', 'synagogue', 'transit_station', 'travel_agency', 'zoo']
 
+
+locations = ['Albania', 'Algeria', 'Argentina', 'Armenia', 'Australia',
+             'Austria', 'Azerbaijan', 'Bahrain', 'Bangladesh', 'Barbados',
+             'Belgium', 'Bolivia', 'Botswana', 'Brazil', 'Bulgaria', 'Cambodia',
+             'Cameroon', 'Canada', 'Chile', 'China', 'Colombia', 'Costa Rica',
+             'Croatia', 'Cyprus', 'Denmark', 'Dominican Republic', 'Ecuador',
+             'Egypt', 'El Salvador', 'Estonia', 'Ethiopia', 'Finland', 'France',
+             'Georgia', 'Germany', 'Ghana', 'Greece', 'Guatemala', 'Honduras',
+             'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran',
+             'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan',
+             'Kazakhstan', 'Kenya', 'Kuwait', 'Latvia', 'Lebanon', 'Lithuania',
+             'Luxembourg', 'Malaysia', 'Malta', 'Mauritius', 'Mexico',
+             'Moldova', 'Mongolia', 'Montenegro', 'Morocco', 'Myanmar', 'Nepal',
+             'Netherlands', 'New Zealand', 'Nicaragua', 'Nigeria',
+             'North Macedonia', 'Norway', 'Oman', 'Pakistan', 'Panama',
+             'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar',
+             'Romania', 'Russia', 'Rwanda', 'Saudi Arabia', 'Senegal', 'Serbia',
+             'Seychelles', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa',
+             'South Korea', 'Spain', 'Sri Lanka', 'Suriname', 'Sweden',
+             'Switzerland', 'Taiwan', 'Tanzania', 'Thailand', 'Tunisia',
+             'Turkey', 'Uganda', 'Ukraine', 'United Arab Emirates',
+             'United Kingdom', 'United States', 'Uruguay', 'Venezuela',
+             'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe']
+
+
+# read in continent file
+continent_dictionary = pd.read_csv("data/ContinentCountry.csv")
+
 app = Dash(__name__)
 
-app.layout = html.Div(className='block ml-4 mt-4', children=[
-    html.Div(className='columns', children=[
-        html.Div(className='column is-one-third', children=[
-            html.Div(className='box', children=[
-                html.Div(className='block',
-                         children=[html.P('HolidayPlanner', className='has-text-weight-bold is-size-3')]),
-                html.Div(className='block', children=[
-                    html.P(
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore "
-                        "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
-                        "aliquip ex ea commodo consequat.")]),
-                html.Div(className='block', children=[
+app.layout = html.Div(children=[
+    html.Div(className='block mx-4 mt-4', children=[
+        html.Div(className='columns', children=[
+            html.Div(className='column is-one-third is-flex ', children=[
+                html.Div(className='box', children=[
+                    html.Div(className='block',
+                         children=[
+                             html.P('HolidayPlanner',
+                                    className='has-text-weight-bold is-size-3'),
+                             html.P(
+                                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore "
+                                 "et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut "
+                                 "aliquip ex ea commodo consequat.")
+                         ]),
                     html.Div(className='block', children=[
-                        html.Label('Covid Concern', className='has-text-weight-semibold'),
-                        dcc.Slider(0, 2, 1, value=1,
-                                   marks={
-                                       0: {'label': 'Low'},
-                                       1: {'label': 'Medium'},
-                                       2: {'label': 'High'}
-                                   },
-                                   included=False)
+                        html.Label(
+                            'Where would you like to go?', className='has-text-weight-medium is-size-5'),
+                        dcc.Dropdown(locations, multi=False, id="location_select")
                     ]),
                     html.Div(className='block', children=[
-                        html.Label('Cost', className='has-text-weight-semibold'),
-                        dcc.Slider(0, 2, 1, value=1,
-                                   marks={
-                                       0: {'label': 'Budget'},
-                                       1: {'label': 'Mid Range'},
-                                       2: {'label': 'Luxury'}
-                                   },
-                                   included=False)
-                    ])
-                ]),
-
-                html.Div(className='block', children=[
-                    html.Label('Continents', className='has-text-weight-semibold'),
-                    dcc.Dropdown(['Asia', 'Africa', 'Oceania', 'Europe', 'North America', 'South America'],
-                                 multi=True)
-                ]),
-                html.Div(className='block', children=[
-                    html.Label('Interests', className='has-text-weight-semibold'),
-                    html.Label(''),
-                    dcc.Dropdown(attraction_types,
-                                 multi=True)
+                        html.Div(className='block', children=[
+                            html.Label(
+                                'Continents', className='has-text-weight-semibold'),
+                            dcc.Dropdown(['Asia', 'Africa', 'Oceania', 'Europe', 'North America', 'South America'],
+                                         multi=True, id="continent_select")
+                        ]),
+                        html.Div(className='block', children=[
+                            html.Label(
+                                'Interests', className='has-text-weight-semibold'),
+                            html.Label(''),
+                            dcc.Dropdown(attraction_types,
+                                         multi=True)
+                        ]),
+                        html.Div(className='block', children=[
+                            html.Label('Covid Concern',
+                                       className='has-text-weight-semibold'),
+                            dcc.Slider(0, 2, 1, value=1,
+                                       marks={
+                                           0: {'label': 'Low'},
+                                           1: {'label': 'Medium'},
+                                           2: {'label': 'High'}
+                                       },
+                                       included=False)
+                        ]),
+                        html.Div(className='block', children=[
+                            html.Label(
+                                'Cost', className='has-text-weight-semibold'),
+                            dcc.Slider(0, 2, 1, value=1,
+                                       marks={
+                                           0: {'label': 'Budget'},
+                                           1: {'label': 'Mid Range'},
+                                           2: {'label': 'Luxury'}
+                                       },
+                                       included=False)
+                        ])
+                    ]),
                 ])
+            ]),
+            html.Div(className='column is-two-thirds', children=[
+                html.Div(className='box', children=[
+                    html.Div(className="block", children=[
+                         dcc.Graph(figure=world_map)]),
+                    html.Div(className="box", children=[
+                        html.Div(className="columns", children=[
+                            html.Div(className="column is-two-fifth", children=[
+                                html.Img(src="https://picsum.photos/128",
+                                     className="image is-128x128")
+                            ]),
+                            html.Div(className="column", children=[
+                                html.Div(className="block", children=[
+                                    html.P(
+                                        'Country 1', className='has-text-weight-bold is-size-5'),
+                                    html.P(
+                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+                                ])
+                            ]),
+                            html.Div(className="column pb-0 mb-0", children=[
+                                html.Div(className="block pb-0 mb-0", children=[
+                                    html.Div(className="columns pb-0 mb-0", children=[
+                                        html.Div(className="column is-one-fifth is-flex is-align-content-flex-start pb-0 mb-0", children=[
+                                            html.P(
+                                                '2', className='has-text-weight-bold is-size-4')
+                                        ]),
+                                        html.Div(className="column pb-0 mb-0", children=[
+                                            html.Img(src="https://picsum.photos/96",
+                                                 className="image is-96x96"),
+                                        ]),
+                                    ]),
+                                    html.Div(className="block is-flex is-justify-content-right", children=[
+                                        html.P(
+                                            'Country 2', className='has-text-weight-bold is-size-6 mx-0 my-0 px-0 py-0')
+                                    ])
+                                ])
+                            ]),
+                            html.Div(className="column pb-0 mb-0", children=[
+                                html.Div(className="block pb-0 mb-0", children=[
+                                    html.Div(className="columns pb-0 mb-0", children=[
+                                        html.Div(className="column is-one-fifth is-flex is-align-content-flex-start pb-0 mb-0", children=[
+                                            html.P(
+                                                '3', className='has-text-weight-bold is-size-4')
+                                        ]),
+                                        html.Div(className="column pb-0 mb-0", children=[
+                                            html.Img(src="https://picsum.photos/96",
+                                                 className="image is-96x96"),
+                                        ]),
+                                    ]),
+                                    html.Div(className="block is-flex is-justify-content-right", children=[
+                                        html.P(
+                                            'Country 3', className='has-text-weight-bold is-size-6 mx-0 my-0 px-0 py-0')
+                                    ])
+                                ])
+                            ]),
+                            html.Div(className="column pb-0 mb-0", children=[
+                                html.Div(className="block pb-0 mb-0", children=[
+                                    html.Div(className="columns pb-0 mb-0", children=[
+                                        html.Div(className="column is-one-fifth is-flex is-align-content-flex-start pb-0 mb-0", children=[
+                                            html.P(
+                                                '4', className='has-text-weight-bold is-size-4')
+                                        ]),
+                                        html.Div(className="column pb-0 mb-0", children=[
+                                            html.Img(src="https://picsum.photos/96",
+                                                 className="image is-96x96"),
+                                        ]),
+                                    ]),
+                                    html.Div(className="block is-flex is-justify-content-right", children=[
+                                        html.P(
+                                            'Country 4', className='has-text-weight-bold is-size-6 mx-0 my-0 px-0 py-0')
+                                    ])
+                                ])
+                            ]),
+                            html.Div(className="column pb-0 mb-0", children=[
+                                html.Div(className="block pb-0 mb-0", children=[
+                                    html.Div(className="columns pb-0 mb-0", children=[
+                                        html.Div(className="column is-one-fifth is-flex is-align-content-flex-start pb-0 mb-0", children=[
+                                            html.P(
+                                                '5', className='has-text-weight-bold is-size-4')
+                                        ]),
+                                        html.Div(className="column pb-0 mb-0", children=[
+                                            html.Img(src="https://picsum.photos/96",
+                                                 className="image is-96x96"),
+                                        ]),
+                                    ]),
+                                    html.Div(className="block is-flex is-justify-content-right", children=[
+                                        html.P(
+                                            'Country 5', className='has-text-weight-bold is-size-6 mx-0 my-0 px-0 py-0')
+                                    ])
+                                ])
+                            ])
+                        ])
+
+                    ]),
+                ]),
             ])
         ]),
-        html.Div(className='column is-two-thirds', children=[
-            html.Div(className='box', children=[
-                html.Div(className="block", children=[dcc.Graph(figure=world_map)]),
-                html.Div(className="box", children=[
-                    html.Div(className="columns", children=[
-                        html.Div(className="column is-two-fifth", children=[
-                            html.Img(src="https://picsum.photos/128", className="image is-128x128")
-                        ]),
-                        html.Div(className="column", children=[
-                            html.Div(className="block", children=[
-                                html.P('Country 1', className='has-text-weight-bold is-size-5'),
-                                html.P(
-                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
-                            ])
-                        ]),
-                        html.Div(className="column", children=[
-                            html.Img(src="https://picsum.photos/96", className="image is-96x96"),
-                            html.P('Country 2', className='has-text-weight-bold is-size-6')
-                        ]),
-                        html.Div(className="column", children=[
-                            html.Img(src="https://picsum.photos/96", className="image is-96x96"),
-                            html.P('Country 3', className='has-text-weight-bold is-size-6')
-                        ]),
-                        html.Div(className="column", children=[
-                            html.Img(src="https://picsum.photos/96", className="image is-96x96"),
-                            html.P('Country 4', className='has-text-weight-bold is-size-6')
-                        ]),
-                        html.Div(className="column", children=[
-                            html.Img(src="https://picsum.photos/96", className="image is-96x96"),
-                            html.P('Country 5', className='has-text-weight-bold is-size-6')
-                        ])
-                    ])
+    ]),
 
-                ]),
-            ]),
+    html.Footer(className="footer my-0 py-4", children=[
+        html.Div(className="content has-text-centered", children=[
+            html.P("DATA3888 Pty. Ltd.")
         ])
     ])
 ])
-html.Footer(className="footer", children=[
-    html.Div(className="content has-text-centered", children=[
-        html.P("Best Team Pty. Ltd.")
-    ])
-])
+
+@app.callback(
+    Output('continent_select', 'value'),
+    Input('location_select', 'value')
+)
+def set_location_values(selected_location):
+    if selected_location is not None:
+        return continent_dictionary[continent_dictionary['Country'] == selected_location]["Continent"].item()
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
