483187e13aed92b29099b6d3efbc5af4a21f7bdf
Eve Fernando <eve@eandshome.net>
2022-05-15 11:13:14 +1000

    Changes continents to regions

--- analytics.py
+++ analytics.py
@@ -11,14 +11,19 @@ random.seed(3888)
 
 def generate_country_df(countries_data: pd.DataFrame,
                         country: str,
-                        continents: List[str],
+                        regions: List[str],
                         interests: List[str]) -> pd.DataFrame:
     """Generates a Pandas DataFrame containing information about a particular country, including its 5 nearest neighbours.
 
     Args:
         countries_data (pd.DataFrame): DataFrame returned by integrate_all_data().
         country (str): Country selected by user.
-        continents (List[str]): Can be any of the continent names in the OWID dataset (e.g., 'Asia', 'Europe').
+        regions (List[str]): A list containing 1 or more regions (see list below).
+            Region names:
+            - Asia-Pacific
+            - Americas
+            - Europe
+            - Africa
         interests (List[str]): Interest(s) selected by user in UI. (These are weighted more in 5-NN.)
                                Interest options:
                                - covid
@@ -34,22 +39,26 @@ def generate_country_df(countries_data: pd.DataFrame,
                                - wildlife
 
     Returns:
-        pandas.DataFrame: DataFrame containing information about a particular country, including its 5 nearest neighbours.
+        pandas.DataFrame: DataFrame containing information about a particular country, including its 5 nearest neighbours. Returns None if
+            - the user hasn't selected any interests/variable groups, or
+            - the user hasn't selected any regions.
     """
+    if len(interests) == 0 or len(regions) == 0:
+        return None
+
+    continents = convert_regions_to_continents(regions)
+
     cols_of_interest = convert_interests_to_cols(interests)
     weightings = generate_feature_weightings_dict(cols_of_interest)
 
     medians_scaled, medians, data_no_quant = prepare_data_for_nn(countries_data, country, continents, weightings)
-    
-    if medians.shape[0] != 0:
-        return generate_final_df_w_nn(country, medians_scaled, medians, data_no_quant)
 
-    return None
+    return generate_final_df_w_nn(country, medians_scaled, medians, data_no_quant)
 
 def main():
     # Example usage of function above
     countries_data = integrate_all_data()
-    print(generate_country_df(countries_data, "France", ['Europe', 'Oceania'], ['covid']))
+    print(generate_country_df(countries_data, "France", ['Asia-Pacific'], ['covid']))
 
 if __name__ == "__main__":
     main()
